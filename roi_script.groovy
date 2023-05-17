9569cec9961603ee98e55b0918b194f605232526
Katerina Holan <katholan@gmail.com>
2023-05-17 14:33:12 -0500

    Add files via upload

---
+++ roi_script.groovy
@@ -0,0 +1,15 @@
+#@ RoiManager RoiManager
+#@ ResultsTable ResultsTable
+import ij.IJ
+path_to_output = 'C:/Users/Katerina/Desktop/LeafPustuleData_v2/testing_leaves/'
+ResultsTable.deleteRows(0, 999999)
+annotations = RoiManager.getROIs()
+annotations.each{ key, value ->
+	ResultsTable.incrementCounter()
+	p = value.getPolygon()
+	ResultsTable.addValue("x", p.xpoints.join(",") + "," + p.xpoints[0])
+	ResultsTable.addValue("y", p.ypoints.join(",") + "," + p.ypoints[0])}
+img_filename = IJ.getImage().toString()
+base_filename = img_filename.substring(5, img_filename.indexOf('.tif'))
+//ResultsTable.show(base_filename)
+ResultsTable.save(path_to_output + base_filename + '.csv')
